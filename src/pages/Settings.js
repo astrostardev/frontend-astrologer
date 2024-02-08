ee58179dcf0347efffca09414e21fd76df733c52
astrostardev <153346634+astrostardev@users.noreply.github.com>
2024-02-08 12:16:49 +0530

    Add files via upload

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,62 +1,123 @@
-import "../Stylesheets/Settings.css"
-import { alpha, styled } from '@mui/material/styles';
-import Switch from '@mui/material/Switch';
+import "../Stylesheets/Settings.css";
+import { alpha, styled } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
 import Sidebar from "../components/Sidebar";
 import Offcanvas from "../components/Offcanvas";
 import MetaData from "./MetaData";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Button from "react-bootstrap/Button";
 
 function Settings() {
+  const { astrologer = [] } = useSelector((state) => state.astroState);
+  const CustomSwitch = styled(Switch)(({ theme }) => ({
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      color: "#FFCB11",
+      "&:hover": {
+        backgroundColor: alpha("#FFCB11", theme.palette.action.hoverOpacity),
+      },
+    },
+    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+      backgroundColor: "#FFCB11",
+    },
+  }));
 
+  const label = { inputProps: { "aria-label": "Color switch demo" } };
 
-    const CustomSwitch = styled(Switch)(({ theme }) => ({
-        '& .MuiSwitch-switchBase.Mui-checked': {
-            color: "#FFCB11",
-            '&:hover': {
-                backgroundColor: alpha("#FFCB11", theme.palette.action.hoverOpacity),
-            },
-        },
-        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
-            backgroundColor: "#FFCB11",
-        },
-    }));
+  const [voiceCallEnabled, setVoiceCallEnabled] = useState(
+    astrologer[0].callAvailable
+  );
+  const [textChatEnabled, setTextChatEnabled] = useState(
+    astrologer[0].chatAvailable
+  );
+  const [emergencyCallEnabled, setEmergencyCallEnabled] = useState(
+    astrologer[0].emergencyCallAvailable
+  );
 
-    const label = { inputProps: { 'aria-label': 'Color switch demo' } };
-    return (
-        <>
-        <MetaData title={'Astro5Star-Contributor'} />
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-             <div id="fixedbar">
+    try {
+      const availableStatus = {
+        callAvailable: voiceCallEnabled,
+        chatAvailable: textChatEnabled,
+        emergencyCallAvailable: emergencyCallEnabled,
+      };
+
+      const response = await fetch(
+        `http://127.0.0.1:8001/api/v1/astrologer/available/${astrologer[0]?._id}`,
+
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(availableStatus),
+        }
+      );
+
+      console.log(response);
+      if (response.ok === false) {
+        alert("failed");
+      } else {
+        alert("sucess");
+      }
+    } catch (error) {
+      console.error("Error during course update:", error);
+    }
+  };
+
+  return (
+    <>
+      <MetaData title={"Astro5Star-Contributor"} />
+      <div id="fixedbar">
         <Sidebar />
       </div>
       <div id="offcanvas">
         <Offcanvas />
       </div>
-
       <div className="infoContainer">
-            <h4>Settings</h4>
-            <div style={{ height: "3px", width: "40px", backgroundColor: "#FFCB11", borderRadius: "10px", marginTop: "3px" }}></div>
-            <main id="settings">
-                <div>
-                    <span></span>
-                    <p style={{marginTop:"0"}}>Off/On</p>
-                </div>
-                <div>
-                    <p>Voice call</p>
-                    <CustomSwitch {...label} defaultChecked />
-                </div>
-                <div>
-                    <p>Text chat</p>
-                    <CustomSwitch {...label} defaultChecked />
-                </div>
-                <div>
-                    <p>Emergency call</p>
-                    <CustomSwitch {...label} defaultChecked />
-                </div>
-            </main>
-            </div>
-        </>
-    )
+        <h4>Settings</h4>
+        <div className="header_line"></div>
+        <main id="settings">
+          <div>
+            <span></span>
+            <p>Off/On</p>
+          </div>
+          <div>
+            <p>
+              Voice call
+              {/* <span>{astrologer[0]?.callAvailable ? '(available)' : '(not available)'}</span> */}
+            </p>
+            <CustomSwitch
+              {...label}
+              checked={voiceCallEnabled}
+              onChange={() => setVoiceCallEnabled(!voiceCallEnabled)}
+            />
+          </div>
+          <div>
+            <p>Text chat</p>
+            <CustomSwitch
+              {...label}
+              checked={textChatEnabled}
+              onChange={() => setTextChatEnabled(!textChatEnabled)}
+            />
+          </div>
+          <div>
+            <p>Emergency call</p>
+            <CustomSwitch
+              {...label}
+              defaultChecked={emergencyCallEnabled}
+              onChange={() => setEmergencyCallEnabled(!emergencyCallEnabled)}
+            />
+          </div>
+        </main>
+        <Button onClick={onSubmit} className="btn-primery">
+          sumbit
+        </Button>
+      </div>
+    </>
+  );
 }
 
-
-export default Settings
+export default Settings;
